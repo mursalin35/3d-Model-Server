edafa7dbb717518355789a83d2d5abe9fcd0bf2c
mursalin35 <msmursalin35@gmail.com>
2025-11-06 18:23:35 +0600

    fist com

---
+++ index.js
@@ -0,0 +1,151 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const admin = require("firebase-admin");
+require('dotenv').config()
+const serviceAccount = require("./firebase-admin-key.json");
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vx3mlx4.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// middleWere
+const verifyFirebaseToken = (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1]
+  console.log(token)
+    next();
+
+};
+
+async function run() {
+  try {
+    // await client.connect();
+
+    const db = client.db("modelDB");
+    const modelCollection = db.collection("models");
+
+    // find
+    app.get("/models", async (req, res) => {
+      const result = await modelCollection.find().toArray();
+      res.send(result);
+    });
+
+    // findOne
+    app.get("/models/:id", verifyFirebaseToken, async (req, res) => {
+      const { id } = req.params;
+      const result = await modelCollection.findOne({ _id: new ObjectId(id) });
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    // post method
+    // insertOne
+    // insertMany
+    app.post("/models", async (req, res) => {
+      const data = req.body;
+      const result = await modelCollection.insertOne(data);
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    // PUT
+    // updateOne
+    // updateMany
+    app.put("/models/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+      const update = {
+        $set: data,
+      };
+      const result = await modelCollection.updateOne(filter, update);
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    // delete
+    // deleteOne
+    // deleteMany
+    app.delete("/models/:id", async (req, res) => {
+      const { id } = req.params;
+      //   const objectId = new ObjectId(id);
+      //   const filter = { _id: objectId };
+      const result = await modelCollection.deleteOne({ _id: new ObjectId(id) });
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    // latest 6 data
+    // get
+    // find
+    app.get("/latest-models", async (req, res) => {
+      const result = await modelCollection
+        .find()
+        .sort({ created_at: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+
+    // .............................................MongoClient.EventEmitter..................
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
